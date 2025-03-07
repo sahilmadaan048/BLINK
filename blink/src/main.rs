9d9e1bdc61334154c5a5e9d3452b137481f677b8
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-07 14:38:14 +0530

    updated code

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -1,22 +1,8 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-
-    for byte in io::stdin().bytes() {
-        let b = byte.unwrap(); // Extract u8 byte
-        let c = b as char; // Convert to char
-
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:3} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:3} Character: {1:#?}\r", b, c);
-        }
-        println!("{}", c);
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
+    let mut editor = Editor::default(); // Initialize an editor instance
+    editor.run(); // Run the editor
 }
