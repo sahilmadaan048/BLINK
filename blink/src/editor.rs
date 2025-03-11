dca9921a8e4faa0177893ca4b4cfd61fc7a38ff6
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-11 13:42:28 +0530

    caret working fine on key presses

--- blink/src/editor.rs
+++ blink/src/editor.rs
@@ -1,20 +1,29 @@
-use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
-use std::{fmt::format, io::{Error, Result}};
+use core::cmp::min;
+use crossterm::event::{
+    Event::{self, Key},
+    KeyCode::{self, Char},
+    KeyEvent, KeyEventKind, KeyModifiers, read,
+};
+use std::io::Error;
+
 mod terminal;
 use terminal::{Position, Size, Terminal};
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Copy, Clone, Default)]
+struct Location {
+    x: usize,
+    y: usize,
+}
+#[derive(Default)]
 pub struct Editor {
     should_quit: bool,
+    location: Location,
 }
 
 impl Editor {
-    pub const fn default() -> Self {
-        Self { should_quit: false }
-    }
-
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
         let result = self.repl();
@@ -22,48 +31,99 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<()> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
-            if let Ok(event) = read() {
-                self.evaluate_event(&event);
-            }
+            let event = read()?;
+            self.evaluate_event(&event)?;
         }
         Ok(())
     }
 
-    fn evaluate_event(&mut self, event: &Event) {
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = Terminal::size()?;
+        match key_code {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            KeyCode::PageUp => {
+                y = 0;
+            }
+            KeyCode::PageDown => {
+                y = height.saturating_sub(1);
+            }
+            KeyCode::Home => {
+                x = 0;
+            }
+            KeyCode::End => {
+                x = width.saturating_sub(1);
+            }
+            _ => (),
+        }
+        self.location = Location { x, y };
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
         if let Key(KeyEvent {
-            code, modifiers, ..
-        }) = *event
+            code,
+            modifiers,
+            kind: KeyEventKind::Press,
+            ..
+        }) = event
         {
             match code {
-                Char('q') if modifiers == KeyModifiers::CONTROL => {
+                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.should_quit = true;
+                }
+                KeyCode::Up
+                | KeyCode::Down
+                | KeyCode::Left
+                | KeyCode::Right
+                | KeyCode::PageDown
+                | KeyCode::PageUp
+                | KeyCode::End
+                | KeyCode::Home => {
+                    self.move_point(*code)?;
                 }
                 _ => (),
             }
         }
+        Ok(())
     }
 
-    fn refresh_screen(&self) -> Result<()> {
-        Terminal::hide_cursor()?;
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_caret()?;
+        Terminal::move_caret_to(Position::default())?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            Terminal::print("Goodbye.\r\n");
+            Terminal::print("Goodbye!\r\n");
         } else {
             self.draw_rows()?;
-            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
+            Terminal::move_caret_to(Position {
+                col: self.location.x,
+                row: self.location.y,
+            })?;
         }
-        Terminal::show_cursor()?;
+        Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
     }
 
-    fn draw_welcome_message() -> Result<()> {
+    fn draw_welcome_message() -> Result<(), Error> {
         let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
         let width = Terminal::size()?.width;
         let len = welcome_message.len();
@@ -80,19 +140,19 @@ impl Editor {
         Ok(())
     }
 
-    fn draw_empty_row()-> Result<()> {
-            Terminal::print("~")?;
-            Ok(())
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
     }
 
-    fn draw_rows(&self) -> Result<()> {
-        let Size {height, ..} = Terminal::size()?;
+    fn draw_rows(&self) -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
             // Terminal::print("~");
             if current_row == height / 3 {
                 Self::draw_welcome_message()?;
-            }else {
+            } else {
                 Self::draw_empty_row()?;
             }
             if current_row.saturating_add(1) < height {
--- blink/src/editor/terminal.rs
+++ blink/src/editor/terminal.rs
@@ -10,10 +10,10 @@ use crossterm::{queue, Command};
      pub height: usize,
      pub width: usize,
  }
- #[derive(Copy, Clone)]
+ #[derive(Copy, Clone, Default)]
  pub struct Position {
-     pub x: usize,
-     pub y: usize,
+     pub col: usize,
+     pub row: usize,
  }
 pub struct Terminal;
 
@@ -27,7 +27,6 @@ impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position{x: 0,y:  0})?;
         Self::execute()?;
         Ok(())
     }
@@ -42,18 +41,18 @@ impl Terminal {
         Ok(())
     }
     
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
         #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
-        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
     
-    pub fn hide_cursor() -> Result<(), Error> {
+    pub fn hide_caret() -> Result<(), Error> {
         Self::queue_command(Hide)?;
         Ok(())
     }
     
-    pub fn show_cursor() -> Result<(), Error> {
+    pub fn show_caret() -> Result<(), Error> {
         Self::queue_command(Show)?;
         Ok(())
     }
