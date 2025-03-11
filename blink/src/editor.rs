61db866b89effee7fa0f3492e6fb411d95cae962
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-11 10:09:25 +0530

    did assignment 2

--- blink/src/editor.rs
+++ blink/src/editor.rs
@@ -1,15 +1,17 @@
-use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyModifiers, read};
-use crossterm::terminal::{enable_raw_mode};
+use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use std::io::Result;
 mod terminal;
-use terminal::Terminal;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
 }
+
 impl Editor {
     pub const fn default() -> Self {
         Self { should_quit: false }
     }
+
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
         let result = self.repl();
@@ -17,34 +19,54 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
+    fn repl(&mut self) -> Result<()> {
         loop {
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
-            let event = read();
-        }
-        Ok(())
-    }
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        if self.should_quit {
-            Terminal::clear_screen()?;
-            print!("Goodbye.\r\n");
-        } else {
-            Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            if let Ok(event) = read() {
+                self.evaluate_event(&event);
+            }
         }
         Ok(())
     }
 
-    fn draw_rows() -> Result<(), std::io::Error> { 
-        let height = Terminal::size()?.1;
-        for current_row in 0..height { 
-            print!("~"); 
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = *event
+        {
+            match code {
+                Char('q') if modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<()> {
+        Terminal::hide_cursor()?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            Terminal::print("Goodbye.\r\n");
+        } else {
+            self.draw_rows()?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
+        }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
+        Ok(())
+    }
+
+    fn draw_rows(&self) -> Result<()> {
+        let (height, width) = Terminal::size()?;
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            Terminal::print("~");
             if current_row + 1 < height {
-                print!("\r\n"); 
+                Terminal::print("\r\n");
             }
         }
         Ok(())
