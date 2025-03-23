4c97f735e1700d93ff7f0c4159b6788793b6bd6f
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-23 12:44:32 +0530

    10 scrolling

--- blink/target/debug/deps/blink-3875ad0c6d635533.d
+++ blink/target/debug/deps/blink-3875ad0c6d635533.d
@@ -1,12 +1,14 @@
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-3875ad0c6d635533.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-3875ad0c6d635533.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
 
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-3875ad0c6d635533.d: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-3875ad0c6d635533.d: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
 
 src\main.rs:
 src\editor.rs:
 src\editor\terminal.rs:
 src\editor\view.rs:
 src\editor\view\buffer.rs:
+src\editor\view\location.rs:
+src\editor\view\line.rs:
 
 # env-dep:CARGO_PKG_NAME=blink
 # env-dep:CARGO_PKG_VERSION=0.1.0
--- blink/target/debug/deps/blink-39cfb8e8b0ca3482.d
+++ blink/target/debug/deps/blink-39cfb8e8b0ca3482.d
@@ -1,12 +1,14 @@
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-39cfb8e8b0ca3482.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-39cfb8e8b0ca3482.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
 
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-39cfb8e8b0ca3482.d: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-39cfb8e8b0ca3482.d: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
 
 src\main.rs:
 src\editor.rs:
 src\editor\terminal.rs:
 src\editor\view.rs:
 src\editor\view\buffer.rs:
+src\editor\view\location.rs:
+src\editor\view\line.rs:
 
 # env-dep:CARGO_PKG_NAME=blink
 # env-dep:CARGO_PKG_VERSION=0.1.0
