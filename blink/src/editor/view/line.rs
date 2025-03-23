26fc982719885817a7b98892273518c3d2047beb
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-23 14:23:59 +0530

    complex deletion

--- blink/src/editor/view/line.rs
+++ blink/src/editor/view/line.rs
@@ -1,5 +1,4 @@
-use std::ops::Range;
-
+use std::{fmt, ops::Range};
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
@@ -142,5 +141,22 @@ impl Line {
             }
         }
         self.fragments = Self::str_to_fragments(&result);
+    }
+
+    pub fn append(&mut self, other: &Self) {
+        let mut concat = self.to_string();
+        concat.push_str(&other.to_string());
+        self.fragments = Self::str_to_fragments(&concat);
+    }
+}
+
+impl fmt::Display for Line {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        let result: String = self
+            .fragments
+            .iter()
+            .map(|fragment| fragment.grapheme.clone())
+            .collect();
+        write!(formatter, "{result}")
     }
 }
