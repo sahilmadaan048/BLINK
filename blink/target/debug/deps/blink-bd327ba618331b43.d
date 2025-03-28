04e6874ad07f97618e70ce9bc6d932e7b4f681e7
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-28 14:10:51 +0530

    grapheme search

--- blink/target/debug/deps/blink-bd327ba618331b43.d
+++ blink/target/debug/deps/blink-bd327ba618331b43.d
@@ -1,9 +1,9 @@
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-bd327ba618331b43.rmeta: src\main.rs src\editor.rs src\editor\annotatedstring\mod.rs src\editor\annotatedstring\annotationtype.rs src\editor\annotatedstring\annotation.rs src\editor\annotatedstring\annotatedstringpart.rs src\editor\annotatedstring\annotatedstringiterator.rs src\editor\command\mod.rs src\editor\command\movecommand.rs src\editor\command\system.rs src\editor\command\edit.rs src\editor\uicomponents\mod.rs src\editor\uicomponents\commandbar.rs src\editor\uicomponents\messagebar.rs src\editor\uicomponents\statusbar.rs src\editor\uicomponents\view\mod.rs src\editor\uicomponents\view\buffer.rs src\editor\uicomponents\view\searchdirection.rs src\editor\uicomponents\view\location.rs src\editor\uicomponents\view\fileinfo.rs src\editor\uicomponents\view\searchinfo.rs src\editor\uicomponents\uicomponent.rs src\editor\documentstatus.rs src\editor\line\mod.rs src\editor\line\graphemewidth.rs src\editor\line\textfragment.rs src\editor\position.rs src\editor\size.rs src\editor\terminal\mod.rs src\editor\terminal\attribute.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\libblink-bd327ba618331b43.rmeta: src\main.rs src\editor\mod.rs src\editor\annotatedstring\mod.rs src\editor\annotatedstring\annotationtype.rs src\editor\annotatedstring\annotation.rs src\editor\annotatedstring\annotatedstringpart.rs src\editor\annotatedstring\annotatedstringiterator.rs src\editor\command\mod.rs src\editor\command\movecommand.rs src\editor\command\system.rs src\editor\command\edit.rs src\editor\documentstatus.rs src\editor\line\mod.rs src\editor\line\graphemewidth.rs src\editor\line\textfragment.rs src\editor\terminal\mod.rs src\editor\terminal\attribute.rs src\editor\uicomponents\mod.rs src\editor\uicomponents\commandbar.rs src\editor\uicomponents\messagebar.rs src\editor\uicomponents\statusbar.rs src\editor\uicomponents\view\mod.rs src\editor\uicomponents\view\buffer.rs src\editor\uicomponents\view\searchdirection.rs src\editor\uicomponents\view\fileinfo.rs src\editor\uicomponents\view\searchinfo.rs src\editor\uicomponents\uicomponent.rs src\prelude\mod.rs src\prelude\position.rs src\prelude\size.rs src\prelude\location.rs
 
-E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-bd327ba618331b43.d: src\main.rs src\editor.rs src\editor\annotatedstring\mod.rs src\editor\annotatedstring\annotationtype.rs src\editor\annotatedstring\annotation.rs src\editor\annotatedstring\annotatedstringpart.rs src\editor\annotatedstring\annotatedstringiterator.rs src\editor\command\mod.rs src\editor\command\movecommand.rs src\editor\command\system.rs src\editor\command\edit.rs src\editor\uicomponents\mod.rs src\editor\uicomponents\commandbar.rs src\editor\uicomponents\messagebar.rs src\editor\uicomponents\statusbar.rs src\editor\uicomponents\view\mod.rs src\editor\uicomponents\view\buffer.rs src\editor\uicomponents\view\searchdirection.rs src\editor\uicomponents\view\location.rs src\editor\uicomponents\view\fileinfo.rs src\editor\uicomponents\view\searchinfo.rs src\editor\uicomponents\uicomponent.rs src\editor\documentstatus.rs src\editor\line\mod.rs src\editor\line\graphemewidth.rs src\editor\line\textfragment.rs src\editor\position.rs src\editor\size.rs src\editor\terminal\mod.rs src\editor\terminal\attribute.rs
+E:\Code_Everyday\WebDev PROJECTS\BLINK\blink\target\debug\deps\blink-bd327ba618331b43.d: src\main.rs src\editor\mod.rs src\editor\annotatedstring\mod.rs src\editor\annotatedstring\annotationtype.rs src\editor\annotatedstring\annotation.rs src\editor\annotatedstring\annotatedstringpart.rs src\editor\annotatedstring\annotatedstringiterator.rs src\editor\command\mod.rs src\editor\command\movecommand.rs src\editor\command\system.rs src\editor\command\edit.rs src\editor\documentstatus.rs src\editor\line\mod.rs src\editor\line\graphemewidth.rs src\editor\line\textfragment.rs src\editor\terminal\mod.rs src\editor\terminal\attribute.rs src\editor\uicomponents\mod.rs src\editor\uicomponents\commandbar.rs src\editor\uicomponents\messagebar.rs src\editor\uicomponents\statusbar.rs src\editor\uicomponents\view\mod.rs src\editor\uicomponents\view\buffer.rs src\editor\uicomponents\view\searchdirection.rs src\editor\uicomponents\view\fileinfo.rs src\editor\uicomponents\view\searchinfo.rs src\editor\uicomponents\uicomponent.rs src\prelude\mod.rs src\prelude\position.rs src\prelude\size.rs src\prelude\location.rs
 
 src\main.rs:
-src\editor.rs:
+src\editor\mod.rs:
 src\editor\annotatedstring\mod.rs:
 src\editor\annotatedstring\annotationtype.rs:
 src\editor\annotatedstring\annotation.rs:
@@ -13,6 +13,12 @@ src\editor\command\mod.rs:
 src\editor\command\movecommand.rs:
 src\editor\command\system.rs:
 src\editor\command\edit.rs:
+src\editor\documentstatus.rs:
+src\editor\line\mod.rs:
+src\editor\line\graphemewidth.rs:
+src\editor\line\textfragment.rs:
+src\editor\terminal\mod.rs:
+src\editor\terminal\attribute.rs:
 src\editor\uicomponents\mod.rs:
 src\editor\uicomponents\commandbar.rs:
 src\editor\uicomponents\messagebar.rs:
@@ -20,18 +26,13 @@ src\editor\uicomponents\statusbar.rs:
 src\editor\uicomponents\view\mod.rs:
 src\editor\uicomponents\view\buffer.rs:
 src\editor\uicomponents\view\searchdirection.rs:
-src\editor\uicomponents\view\location.rs:
 src\editor\uicomponents\view\fileinfo.rs:
 src\editor\uicomponents\view\searchinfo.rs:
 src\editor\uicomponents\uicomponent.rs:
-src\editor\documentstatus.rs:
-src\editor\line\mod.rs:
-src\editor\line\graphemewidth.rs:
-src\editor\line\textfragment.rs:
-src\editor\position.rs:
-src\editor\size.rs:
-src\editor\terminal\mod.rs:
-src\editor\terminal\attribute.rs:
+src\prelude\mod.rs:
+src\prelude\position.rs:
+src\prelude\size.rs:
+src\prelude\location.rs:
 
 # env-dep:CARGO_PKG_NAME=blink
 # env-dep:CARGO_PKG_VERSION=0.1.0
